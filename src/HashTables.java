d976677b4e3b7daa5eda7f08c59047b5b69a434b
shamar <weekesshamar38@gmail.com>
2025-03-11 01:01:30 -0400

    set up for project

---
+++ src/HashTables.java
@@ -0,0 +1,20 @@
+public class HashTables {
+    // function for inserting values into the hash table
+    public void hash_insert(int value,int key )
+    {
+        int i= 0;
+    // do while loop to iterate while i !=m the length of the data set
+    }
+    // function for searching a hash table
+    public Object hash_search(int value,int key)
+    {
+        int i = 0;
+        // do while loop for i !m or array at j != null;
+
+        return null;
+    }
+    public int getLinearProbIndex (int key,int i){return 1;}
+ public int getQuadraticProbIndex (int key,int  i){return 1;}
+public int getDoubleHash (int key,int i){}
+
+}
